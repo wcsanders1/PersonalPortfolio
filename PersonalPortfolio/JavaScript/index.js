869064ff8a77d7fd2d769c1e032ede286d319734
William Sanders <william.sanders.dev@gmail.com>
2016-11-14 21:58:14 -0500

    added section for maze program; added javascript

---
+++ PersonalPortfolio/JavaScript/index.js
@@ -0,0 +1,41 @@
+﻿$(window).resize(function () {
+    var pageSize = $("#page-size").css("z-index");
+    var $mazeImage = $("#maze-image");
+
+    if (pageSize >= 1 && pageSize < 4) {
+        $mazeImage.attr("src", "/Images/MazeSize4.PNG");
+    }
+
+    if (pageSize >= 4 && pageSize < 5) {
+        $mazeImage.attr("src", "/Images/MazeSize5.PNG");
+    }
+
+    if (pageSize >= 5 && pageSize < 7) {
+        $mazeImage.attr("src", "/Images/MazeSize6.PNG");
+    }
+
+    if (pageSize >= 7) {
+        $mazeImage.attr("src", "/Images/MazeSize7.PNG");
+    }
+});
+
+$(document).ready(function () {
+    var pageSize = $("#page-size").css("z-index");
+    var $mazeImage = $("#maze-image");
+
+    if (pageSize >= 1 && pageSize < 4) {
+        $mazeImage.attr("src", "/Images/MazeSize4.PNG");
+    }
+
+    if (pageSize >= 4 && pageSize < 5) {
+        $mazeImage.attr("src", "/Images/MazeSize5.PNG");
+    }
+
+    if (pageSize >= 5 && pageSize < 7) {
+        $mazeImage.attr("src", "/Images/MazeSize6.PNG");
+    }
+
+    if (pageSize >= 7) {
+        $mazeImage.attr("src", "/Images/MazeSize7.PNG");
+    }
+});
